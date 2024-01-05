30ae50f924c9bec7ccd5300e30920df4b42316e7
Nikolay Ivanov <eoneof@gmail.com>
2024-01-05 13:19:27 +0300

    clarify naming

--- src/MenuMaker.jsx
+++ src/MenuMaker.jsx
@@ -1,27 +1,27 @@
 /* Add semicolon to prevent bad things from happening to concatenated code. */
 ;
 
+var MenuMaker = /** @class */ (function () {
   var menuBar = app.menus.item('$ID/Main').submenus
 
   /**
-   * @constructor
-   * @arg {{ name: string, items: [{ name: string, onClick(): void, enabled(): boolean }] }} props
+   * @param {{ name: string, items: [{ name: string, onClick(): void, enabled(): boolean }] }} props
    */
-  function $(props) {
+  function /** @constructor */ $(props) {
     this.menuName = props.name
     this.newMenuItems = props.items
 
-    try {menuBar.item(this.menuName).remove()} catch (err) {} // Prevent duplication
+    this.unload() // Prevent possible duplication on instance init
+    menuBar.add(this.menuName, LocationOptions.AFTER, menuBar.lastItem())
 
-    this.menu = menuBar.add(this.menuName, LocationOptions.AFTER, menuBar.lastItem())
-    this.menuList = menuBar.item(this.menuName).menuItems
+    this.menu = menuBar.item(this.menuName).menuItems
 
     var l = this.newMenuItems.length
     for (var i = 0; i < l; i++) {
       var newItem = this.newMenuItems[i]
       var subMenu = app.scriptMenuActions.add(newItem.name)
 
-      this.menuList.add(subMenu)
+      this.menu.add(subMenu)
       newItem.setHandlers(subMenu)
     }
   }
@@ -36,24 +36,19 @@
 }())
 
 var SubMenu = /** @class */ (function () {
-
   /**
-   * @constructor
-   * @arg {{ name: string, onClick(): void, enabled(): boolean }} props
+   * @param {{ name: string, onClick(): void, enabled(): boolean }} props
    */
-  function $(props) {
+  function /** @constructor */ $(props) {
     this.name = props.name
     this.onClick = props.onClick
-    this.enabled = props.enabled || function () {
-      return true
-    }
+    this.enabled = props.enabled || function () {return true}
   }
 
   /**
-   * @method
-   * @arg {MenuItem} subMenu
+   * @param {MenuItem} subMenu
    */
-  $.prototype.setHandlers = function (subMenu) {
+  $.prototype.setHandlers = /** method */ function (subMenu) {
     var self = this
 
     subMenu.eventListeners.add(Event.ON_INVOKE, this.onClick)
